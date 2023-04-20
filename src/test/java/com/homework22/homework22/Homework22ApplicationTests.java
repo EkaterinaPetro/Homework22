4306a9e6c544d4ff946503ed29a9d6921e742f38
PetrovaEkat <katisyn853@gmail.com>
2023-04-20 19:21:27 +0400

    Initial commit

---
+++ src/test/java/com/homework22/homework22/Homework22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework22.homework22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
